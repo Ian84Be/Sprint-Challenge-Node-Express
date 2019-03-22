7d362a6583bfdfa63154e52d18b30441c0281092
Ian Be <ian84be@gmail.com>
2019-03-22 11:54:20 -0500

    CRUD operations for action-router.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 
-// const actionRouter = require('./data/helpers/action-router.js');
+const actionRouter = require('./data/helpers/action-router.js');
 const projectRouter = require('./data/helpers/project-router.js');
 
 const server = express();
@@ -11,7 +11,7 @@ server.use(express.json());
 
 // routing
 server.use('/api/projects', projectRouter);
-// server.use('/api/actions', actionRouter);
+server.use('/api/actions', actionRouter);
 
 server.get('/', (req,res) => {
     res.send(` <h1>${process.env.GREETING}</h1> `)
